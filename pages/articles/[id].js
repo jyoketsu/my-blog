ffc545a7fabcff1256fa5e33e0925be4f7a32111
xujie <xujie@qingtime.com>
2020-05-24 21:28:56 +0800

    first commit

---
+++ pages/articles/[id].js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function () {
+  return (
+    <div className="article">
+      <Head>
+        <title>文章</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      文章详情页
+    </div>
+  );
+}
---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function () {
+  return (
+    <div className="login">
+      <Head>
+        <title>登录-徐杰的博客</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      登录页面
+    </div>
+  );
+}
