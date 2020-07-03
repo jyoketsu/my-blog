fd30d2d8174df856f93c6e63e78460b6311682bb
jyoketsu <jyoketsu@gmail.com>
2020-07-03 09:40:01 +0800

    2020-07-03 09:40:01

--- src/components/article/CodeBlock.tsx
+++ src/components/article/CodeBlock.tsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 // 设置高亮的语言
 import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
-import python from "react-syntax-highlighter/dist/cjs/languages/hljs/python";
+// import python from "react-syntax-highlighter/dist/cjs/languages/hljs/python";
+// import bash from "react-syntax-highlighter/dist/cjs/languages/hljs/bash";
+// import css from "react-syntax-highlighter/dist/cjs/languages/hljs/css";
 // 设置高亮样式
 import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
@@ -14,7 +16,9 @@ interface Props {
 export default function CodeBlock({ language, value = "" }: Props) {
   useEffect(() => {
     SyntaxHighlighter.registerLanguage("javascript", javascript);
-    SyntaxHighlighter.registerLanguage("python", python);
+    // SyntaxHighlighter.registerLanguage("python", python);
+    // SyntaxHighlighter.registerLanguage("bash", bash);
+    // SyntaxHighlighter.registerLanguage("css", css);
   }, []);
 
   return (
