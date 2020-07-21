a325b4b315790f8fdb5df4b3b7c2e06b46e7201e
xujie <xujie@qingtime.com>
2020-07-21 09:12:21 +0800

    2020-07-21 09:12:21

--- src/components/article/CodeBlock.tsx
+++ src/components/article/CodeBlock.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 // import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // 设置高亮的语言
-import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
+// import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
 // import python from "react-syntax-highlighter/dist/cjs/languages/hljs/python";
 // import bash from "react-syntax-highlighter/dist/cjs/languages/hljs/bash";
 // import css from "react-syntax-highlighter/dist/cjs/languages/hljs/css";
